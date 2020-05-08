e4f3b5bcf7441337187a6bc0c3f529bd7aa463f7
andoric <56703336+andoric@users.noreply.github.com>
2020-05-07 20:44:51 -0400

    Untitle Rhythm Game
    first attempt at a rhyhtm game, based off tutorial here:
    https://www.youtube.com/watch?v=cZzf1FQQFA0

---
+++ RhythmGameDemo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+    public bool startPlaying = false;
+    public BeatScroller theBS;
+    public static GameManager instance;
+    public int currentScore, scorePerNote=100;
+    public int greatNoteModifier = 25, perfectNoteModifier = 50;
+    public Text scoreText, multiText, hitText;
+    public int currentMultiplier, multiplierTracker;
+    public int[] multiplierThresholds;
+    private float totalNotes, normalHits, goodHits, perfectHits, missedHits;
+    public GameObject resultsScreen;
+    public Text percentHitText, normalsText, goodsText, perfectText, missesText, rankText, finalScoreText;
+
+
+   // private string arrowBeats = "lldurlldur";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
+
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+                theMusic.Play();
+            }
+        }
+        else
+        {
+            if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+                float totalHit = ((totalNotes - missedHits) / totalNotes) * 100f;
+                normalsText.text = normalHits.ToString();
+                goodsText.text = goodHits.ToString();
+                perfectText.text = perfectHits.ToString();
+                missesText.text = missedHits.ToString();
+                percentHitText.text = totalHit.ToString("F1") + "%";
+                finalScoreText.text = currentScore.ToString();
+            }
+        }
+    }
+    public void NoteHit(int scoreModifier)
+    {
+        
+        if (currentMultiplier - 1 < multiplierThresholds.Length) {
+            multiplierTracker++;
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+        currentScore += (scorePerNote+scoreModifier)*currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
+        multiText.text = "Multiplier: x" + currentMultiplier;
+    }
+    public void NoteMissed()
+    {
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Multiplier: x" + currentMultiplier;
+        missedHits++;
+    }
+
+    public void NormalHit()
+    {
+        
+        NoteHit(0);
+        normalHits++;
+        
+
+    }
+    public void GoodHit()
+    {
+        NoteHit(greatNoteModifier);
+        goodHits++;
+        
+    }
+    public void PerfectHit()
+    {
+        NoteHit(perfectNoteModifier);
+        perfectHits++;
+        
+    }
+}
---
+++ RhythmGameDemo/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                
+                gameObject.SetActive(false);
+                //GameManager.instance.NoteHit();
+                if (Mathf.Abs(transform.position.y) > 0.25f)
+                {
+                    //Debug.Log("Good");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }else if (Mathf.Abs(transform.position.y) > 0.05f)
+                {
+                    //Debug.Log("Great");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else
+                {
+                    //Debug.Log("Perfect");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+            if (gameObject.activeInHierarchy)
+            {
+                GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+            }
+        }
+    }
+}
